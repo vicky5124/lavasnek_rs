9349e92a97c45f593993854a87378e6df3f4228d
zykrahgaming <zykrahgaming@gmail.com>
2021-09-25 12:10:39 +1000

    Update main.py
    Minor change, added description for ping command, spaced out some functions

--- examples/hikari_lightbulb_basic_queue/main.py
+++ examples/hikari_lightbulb_basic_queue/main.py
@@ -7,10 +7,12 @@ import lavasnek_rs
 PREFIX = "!"
 TOKEN = os.environ["DISCORD_TOKEN"]
 
+
 class Data:
     """Global data shared across the entire bot, used to store dashboard values."""
     def __init__(self) -> None:
         self.lavalink: lavasnek_rs.Lavalink = None
+
 
 class MusicBot(lightbulb.Bot):
     """Subclass of lightbulb's Bot object, used to store the lavalink client."""
@@ -21,13 +23,16 @@ class MusicBot(lightbulb.Bot):
 # You may want to enable ALL intents here
 bot = MusicBot(token=TOKEN, command-prefix=PREFIX)
 
+
 @bot.listen(hikari.StartingEvent)
 async def starting_load_extensions(event):
     bot.load_extension('music_plugin')
 
-# "Typical" Ping-Pong command
+
 @bot.command()
+"""Typical Ping-Pong command"""
 async def ping(ctx):
     await ctx.respond("Ping?")
+
 
 bot.run()
