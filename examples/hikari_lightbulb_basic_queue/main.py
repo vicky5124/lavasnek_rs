b74144c64a8a56a3213a8d091e9ef17c151e06ae
zykrahgaming <zykrahgaming@gmail.com>
2021-09-25 11:16:07 +1000

    Create main.py
    Lightbulb bot main file.

---
+++ examples/hikari_lightbulb_basic_queue/main.py
@@ -0,0 +1,28 @@
+import os
+
+import hikari
+import lightbulb
+import lavasnek_rs
+
+PREFIX = "!"
+TOKEN = os.environ["DISCORD_TOKEN"]
+
+class Data:
+    """Global data shared across the entire bot, used to store dashboard values."""
+    def __init__(self) -> None:
+        self.lavalink: lavasnek_rs.Lavalink = None
+
+class MusicBot(lightbulb.Bot):
+    """Subclass of lightbulb's Bot object, used to store the lavalink client."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.data = Data()
+
+# You may want to enable ALL intents here
+bot = MusicBot(token=TOKEN, command-prefix=PREFIX)
+
+@bot.listen(hikari.StartingEvent)
+async def starting_load_extensions(event):
+    bot.load_extension('music_plugin')
+
+bot.run()
